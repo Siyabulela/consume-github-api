c53721628c982f36a04f5d80dffc82222ef727a4
Siyabulela <siyabulelakhumalo079@gmail.com>
2020-06-30 15:31:14 +0200

    Last commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+const logins = require(`../node_modules/cred.json`);
+
+var pullRequests = [],
+    numberOfPulls,
+    createdDate,
+    i = 0;
+
+function repos(repoName, dateStart, dateEnd) {
+    axios({
+            method: "get",
+            url: `https://api.github.com/repos/Umuzi-org/${repoName}/pulls?state=all`,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            auth: {
+                username: logins[0].user,
+                password: logins[0].pass,
+            },
+        })
+        .then((response) => {
+            numberOfPulls = response.data[0].number;
+
+            var dateFrom = dateStart.split("/");
+            var dateTo = dateEnd.split("/");
+            dateStart = new Date(dateFrom);
+            dateEnd = new Date(dateTo);
+
+            for (i = 0; i < numberOfPulls; i++) {
+                createdDate = new Date(response.data[i].created_at);
+
+                if (createdDate > dateStart && createdDate < dateEnd) {
+                    pullRequests[i] = response.data[i];
+                }
+            }
+            console.log(pullRequests);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+repos(`Siyabulela-Khumalo-266-string-calculator`, `05/20/2020`, `06/29/2020`);
+//DATE FORMAT - MM/DD/YYYY
